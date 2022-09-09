64cbb170d89750373b7ab6f6c3f2a0b54be74ae5
ZR21080178-闫志浩 <yanzhihao@zhongruigroup.com>
2022-09-09 11:44:49 +0800

    update

--- src/main/java/com/blog/server/BlogServerApplication.java
+++ src/main/java/com/blog/server/BlogServerApplication.java
@@ -4,8 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.blog"})
 @MapperScan("com.blog.server.mapper")
 public class BlogServerApplication {
 
