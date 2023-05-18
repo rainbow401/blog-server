12135ffb70f9c27460e8357fcaed0ccf79ef8c2b
闫志浩 <yanzhihao@zhongruigroup.com>
2023-05-18 10:06:23 +0800

    sql update

--- sql/0822.sql
+++ sql/0822.sql
@@ -5,7 +5,7 @@ create table blog.article
     title       varchar(100)                        not null comment '标题',
     content     text                                null comment '内容',
     create_by   bigint                              not null,
-    create_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    create_time timestamp default CURRENT_TIMESTAMP not null,
     update_by   bigint                              not null,
     update_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
     deleted     tinyint   default 0                 not null
@@ -16,7 +16,7 @@ create table blog.article_comment
     article_id  bigint                              not null comment '文章id',
     comment_id  bigint                              not null comment '评论id',
     create_by   bigint                              not null,
-    create_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    create_time timestamp default CURRENT_TIMESTAMP not null,
     update_by   bigint                              not null,
     update_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
     deleted     tinyint   default 0                 not null,
@@ -29,7 +29,7 @@ create table blog.article_tag
     article_id  bigint                              not null comment '文章id',
     tag_id      bigint                              not null comment '标签id',
     create_by   bigint                              not null,
-    create_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    create_time timestamp default CURRENT_TIMESTAMP not null,
     update_by   bigint                              not null,
     update_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
     deleted     tinyint   default 0                 not null,
@@ -43,7 +43,7 @@ create table blog.comment
         primary key,
     content     text                                not null comment '内容',
     create_by   bigint                              not null,
-    create_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    create_time timestamp default CURRENT_TIMESTAMP not null,
     update_by   bigint                              not null,
     update_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
     deleted     tinyint   default 0                 not null
@@ -55,7 +55,7 @@ create table blog.message
         primary key,
     content     mediumtext                          not null comment '内容',
     create_by   bigint                              not null,
-    create_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    create_time timestamp default CURRENT_TIMESTAMP not null,
     update_by   bigint                              not null,
     update_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
     deleted     tinyint   default 0                 not null
@@ -67,7 +67,7 @@ create table blog.tag
         primary key,
     name        varchar(100)                        not null comment '名称',
     create_by   bigint                              not null,
-    create_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    create_time timestamp default CURRENT_TIMESTAMP not null,
     update_by   bigint                              not null,
     update_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
     deleted     tinyint   default 0                 not null
@@ -80,7 +80,7 @@ create table blog.user
     username    varchar(30)                         not null comment '用户名',
     password    varchar(500)                        null comment '密码',
     create_by   bigint                              not null,
-    create_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    create_time timestamp default CURRENT_TIMESTAMP not null,
     update_by   bigint                              not null,
     update_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
     deleted     tinyint   default 0                 not null
